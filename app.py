32b295176735bc9e708bcb21a36db7f75caa4961
98maz <maz98@pitt.edu>
2026-04-14 19:02:45 -0400

    Cleaned up dashboard recent section

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def home():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    nutrition_entries = Nutrition.query.all()[:5]
-    activity_entries = Activity.query.all()[:5]
+    nutrition_entries = Nutrition.query.all()[:4]  # Need to order by creation time here too
+    activity_entries = Activity.query.filter_by(user_id=current_user.id).order_by(Activity.created_at.desc()).all()[:4]
     return render_template('dashboard.html', nutrition_entries=nutrition_entries, activity_entries=activity_entries)
 
 @app.route('/nutrition', methods=['GET', 'POST'])
